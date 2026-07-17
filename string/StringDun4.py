41eadcff750881395c6ca454fba160935b077721
samirvithlani <samir.vithlani83955@gmail.com>
2026-07-17 20:23:42 +0530

    string func

---
+++ string/StringDun4.py
@@ -0,0 +1,11 @@
+email = "  samir@gmail.com   "
+print(email)
+# email = email.lstrip()
+# email = email.rstrip()
+email = email.strip()
+print(email)
+
+cnt =email.count("a")
+print(cnt)
+
+
---
+++ string/StringFun5.py
@@ -0,0 +1,10 @@
+data = "java is programming lang, programming is programming"
+print(data.index("is")) #rindex
+print(data.find("p"))
+print(data.rfind("a"))
+
+#data = data.replace("programming","coffee")
+data = data.replace("programming","coffee",1)
+print(data)
+
+
