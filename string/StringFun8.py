6ae224103c6fa4d53b11b65cced0825fe5d23a9b
samirvithlani <samir.vithlani83955@gmail.com>
2026-07-17 20:41:47 +0530

    func

---
+++ string/StringFun8.py
@@ -0,0 +1,14 @@
+name = "Mr.whatever"
+name = name.removeprefix("Mr.")
+print(name)
+
+file  = "abc.jpg"
+file = file.removesuffix(".jpg")
+print(file)
+
+print("My Name is {} and i am {} old. and i am useless going {}".format("Wangchuk",60,"home"))
+
+
+
+
+
